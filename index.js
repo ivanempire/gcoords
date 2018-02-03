d735ad43ab487addd92bee8c76340f6318ae4b84
Ivan Melnikov <ivanempire@gmail.com>
2018-02-02 22:18:35 -0500

    WIP commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const https = require("https");
+const DomParser = require("dom-parser");
 
 let API_KEY = "";
 let ENDPOINT_URL = "https://maps.googleapis.com/maps/api/geocode/";
@@ -18,6 +19,7 @@ exports.init = function(apikey, format) {
 
 	API_KEY = apikey;
 	ENDPOINT_URL += dataFormat;
+	DATA_FORMAT = dataFormat;
 };
 
 exports.getCoords = function(inputString) {
@@ -30,39 +32,49 @@ exports.getCoords = function(inputString) {
 		let address = encodeURIComponent(inputString);
 		let requestUrl = ENDPOINT_URL + "?address=" + address + "&key=" + API_KEY;
 
-		makeRequest(requestUrl,"json").then((result) => {
-			resolve({
-				"lat": result.results[0].geometry.location.lat,
-				"lng": result.results[0].geometry.location.lng,
-			});
+		makeRequest(requestUrl,DATA_FORMAT).then((result) => {
+			if(DATA_FORMAT === "json") {
+				resolve({
+					"lat": result.results[0].geometry.location.lat,
+					"lng": result.results[0].geometry.location.lng,
+				});
+			} else {
+				console.log("XML result!");
+			}
 		}).catch((error) => {
 			throw new Error("Request error, API returned: " + error.status);
 		});
 	});
 };
 
-exports.getLocation = function(coords, callback) {
-	if(!coords) {
-		throw new Error("No coordinates specified");
-	}
+exports.getLocation = function(coords) {
+	return new Promise((resolve, reject) => {
+		if(!coords) {
+			throw new Error("No coordinates specified");
+		}
 
-	let point = coords[0]+","+coords[1];
-	let requestUrl = ENDPOINT_URL + "?latlng=" + point + "&key=" + API_KEY;
+		let point = coords[0]+","+coords[1];
+		let requestUrl = ENDPOINT_URL + "?latlng=" + point + "&key=" + API_KEY;
 
 
-	makeRequest(requestUrl,"json").then((result) => {
-		resolve(result.results[0].formatted_address);
-	}).catch((error) => {
+		makeRequest(requestUrl,DATA_FORMAT).then((result) => {
+			if(DATA_FORMAT === "json") {
+				resolve(result.results[0].formatted_address);
+			} else {
+				console.log("XML result!");
+			}
+		}).catch((error) => {
 			throw new Error("Request error, API returned: " + error.status);
+		});
 	});
 };
+
 
 /**
  * Sends a request to the Google Geocode API
  * @param {string} requestUrl - The API url to query.
  * @param {string} dataFormat - The data format to return results as.
  */
-
 function makeRequest(requestUrl, dataFormat) {
 	return new Promise((resolve, reject) => {
 		https.get(requestUrl, (resp) => {
@@ -79,7 +91,9 @@ function makeRequest(requestUrl, dataFormat) {
 					responseObject = JSON.parse(body);
 					requestStatus = responseObject.status;
 				} else {
-					//XML logic
+					let dParser = new DomParser();
+					responseObject = dParser.parseFromString(body);
+					requestStatus = responseObject.getElementsByTagName("status")[0].innerHTML;
 				}
 
 				switch (requestStatus) {
